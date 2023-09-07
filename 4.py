a1175e35f5a20524bd49ead0b58c990eb200a5a8
timosii <holdcarter@holdcarter.ru>
2023-09-07 15:07:18 +0300

    complete 4 task

--- 4.py
+++ 4.py
@@ -1,20 +1,36 @@
 import sys
 import json
+import argparse
 
 
 def count_questions(data: dict):
     # вывести количество вопросов (questions)
-    raise NotImplementedError
+    count = 0
+    for el in data["game"]["rounds"]:
+        count += len(el["questions"])
+
+    print(count)
 
 
 def print_right_answers(data: dict):
     # вывести все правильные ответы (correct_answer)
-    raise NotImplementedError
+    correct_answers = []
+    for el in data["game"]["rounds"]:
+        for i in el["questions"]:
+            correct_answers.append(i["correct_answer"])
+
+    print(correct_answers)
 
 
 def print_max_answer_time(data: dict):
     # вывести максимальное время ответа (time_to_answer)
-    raise NotImplementedError
+    max_time = 0
+    for el in data["game"]["rounds"]:
+        if el["type"] == "classical":
+            for i in el["questions"]:
+                max_time = i["time_to_answer"] if i["time_to_answer"] > max_time else max_time
+    
+    print(max_time)
 
 
 def main(filename):
@@ -27,5 +43,8 @@ def main(filename):
 
 if __name__ == '__main__':
     # передать имя файла из аргументов командной строки
-    filename = "test.json"
+    parser = argparse.ArgumentParser(description="Take filename")
+    parser.add_argument('filename', type=str, help="filename for analysis")
+    filename = parser.parse_args().filename
+    # filename = "test.json"
     main(filename)
